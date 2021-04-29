7e552bc4b37955d4aa148ae1b447bfb43b10529a
木村毅樹 <80314997+takeki-kimura@users.noreply.github.com>
2021-04-29 11:10:42 +0900

    vieｗ cocktailsvalidation

--- db/migrate/20210423023919_create_cocktails.rb
+++ db/migrate/20210423023919_create_cocktails.rb
@@ -1,12 +1,12 @@
 class CreateCocktails < ActiveRecord::Migration[6.0]
   def change
     create_table :cocktails do |t|
-      t.string     :cocktail_name
-      t.integer    :glass_id
-      t.integer    :base_alcohol_id
-      t.integer    :taste_id
-      t.integer    :degree_id
-      t.text       :cocktail_recipe
+      t.string     :cocktail_name,   null: false
+      t.integer    :glass_id,        null: false
+      t.integer    :base_alcohol_id, null: false 
+      t.integer    :taste_id,        null: false
+      t.integer    :degree_id,       null: false
+      t.text       :cocktail_recipe, null: false
       t.references :user, foreign_key: true 
       t.timestamps
     end
